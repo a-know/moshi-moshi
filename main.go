a7da66865083e9e1b8a516766b0614c0e79bd2ba
a-know <a.know.3373@gmail.com>
2018-06-17 14:51:38 +0900

    first implementation

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/a-know/moshi-moshi/handlers"
+	"github.com/go-chi/chi"
+)
+
+const location = "Asia/Tokyo"
+
+func main() {
+	r := chi.NewRouter()
+
+	// timezone
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		loc = time.FixedZone(location, 9*60*60)
+	}
+	time.Local = loc
+
+	r.Get("/heartbeat", handlers.HandleHeartbeat)
+
+	log.Printf("moshi-moshi started.")
+	http.ListenAndServe(":8080", r)
+}
